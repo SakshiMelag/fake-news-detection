28bd03df2b9ed0a2a20f175585a68e748c3f4525
Sakshi Melag <sakshimelag@gmail.com>
2025-02-19 10:03:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PredictForm from "./PredictForm";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1> Fake News Detection </h1>
+      <PredictForm />
+    </div>
+  );
+}
+
+export default App;
+
+
+
